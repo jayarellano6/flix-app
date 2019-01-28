62c8eedecdd2a4a34d04f711dff37aa3c44479c5
Jay Arellano <jayarellano831@icloud.com>
2019-01-28 14:24:44 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Jay Arellano on 1/28/19.
+//  Copyright © 2019 Jay Arellano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
